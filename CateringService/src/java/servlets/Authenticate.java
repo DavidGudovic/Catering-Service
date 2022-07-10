5c6d41a11c54513482620a0e5c5a60a525bae43f
David Gudovic <dgudovic1@gmail.com>
2022-07-11 01:36:39 +0200

    implemented registration use case

--- CateringService/src/java/servlets/Authenticate.java
+++ CateringService/src/java/servlets/Authenticate.java
@@ -1,28 +1,38 @@
 package servlets;
 
+import beans.Korisnik;
+import beans.Rola;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import repository.KorisnikRepository;
 
-/**
- *
- * @author dgudo
- */
-public class Test extends HttpServlet {
+public class Authenticate extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        HttpSession sesija = request.getSession();
-        if (!"logout".equals(request.getParameter("Action"))) {
-            sesija.setAttribute("User", request.getParameter("User"));
-        } else {
-            sesija.invalidate();
+        
+        KorisnikRepository repositorij = new KorisnikRepository();
+        try {
+            Korisnik noviKorisnik = new Korisnik(request.getParameter("adresa"),
+                    request.getParameter("ime"),
+                    request.getParameter("prezime"),
+                    request.getParameter("korisnickoIme"),
+                    request.getParameter("password"),
+                    0, new Rola(3, "Klijent"));
+            repositorij.dodaj(noviKorisnik);
+        } catch (SQLException sqle) {
+            request.setAttribute("msg", "GRESKA");
+            request.getRequestDispatcher("registracija.jsp").forward(request, response);
         }
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+        request.setAttribute("msg", "Uspesna registracija, ulogujte se!");
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+
     }
 
     @Override
