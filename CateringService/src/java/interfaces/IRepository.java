e73ec8484a80d6b08e5c911ffebbf0e2c9a921c9
David Gudovic <dgudovic1@gmail.com>
2022-07-11 01:34:02 +0200

    added IRepository

---
+++ CateringService/src/java/interfaces/IRepository.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IRepository<T>{   // Svaki repozitori implementira tipiziranu verziju ovog interfejsa. 
+    void dodaj(T t) throws SQLException;
+    List<T> getSve() throws SQLException;
+    void izmeni(T oldT, T newT) throws SQLException;
+    void izbrisi(T objekat) throws SQLException;
+}
