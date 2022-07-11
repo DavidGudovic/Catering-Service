fa1e39e4b56b7615f1e3b8980f4e4dbc8cba5420
David Gudovic <dgudovic1@gmail.com>
2022-07-12 01:16:01 +0200

    implemented getSve method

--- CateringService/src/java/repository/ProizvodRepository.java
+++ CateringService/src/java/repository/ProizvodRepository.java
@@ -1,8 +1,14 @@
 package repository;
 
+import beans.Kategorija;
 import beans.Proizvod;
+import database.ConnectionManager;
 import interfaces.IRepository;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,9 +18,33 @@ public class ProizvodRepository implements IRepository<Proizvod> {
     public void dodaj(Proizvod t) {
         }
 
-    @Override
-    public List<Proizvod> getSve() {
-        return null;
+    
+    /** Vraca ArrayList svih proizvoda iz baze  **/
+    @Override    
+    public List<Proizvod> getSve() throws SQLException{
+        Connection con = ConnectionManager.getConnection();
+        List<Proizvod> svi = new ArrayList<>();
+        String sql = "SELECT `ProizvodID`, `NazivProizvoda`, `Opis`, `Slika`, `CenaPoPorciji`, `Program`, `KategorijaID` FROM `proizvodi` WHERE 1";
+        
+        try(PreparedStatement stmt = con.prepareStatement(sql)){
+        ResultSet rs = stmt.executeQuery();
+        
+        while(rs.next()){
+            svi.add(new Proizvod(rs.getInt("ProizvodID"),
+                                rs.getInt("CenaPoPorciji"),
+                                new Kategorija("","",rs.getInt("KategorijaID")),
+                                rs.getString("NazivProizvoda"),
+                                rs.getString("Opis"),
+                                rs.getString("Slika")));
+        }
+        
+        rs.close();
+        return svi;
+        }catch(SQLException sqle){
+            throw sqle;
+        }finally{
+            con.close();
+        }
     }
 
     @Override
