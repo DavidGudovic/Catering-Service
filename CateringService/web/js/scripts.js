188e3e9cc6647f5198a78843c9fe5fd3ab35dac9
David Gudovic <dgudovic1@gmail.com>
2022-07-12 21:46:07 +0200

    implemented and then deleted a short default img script

--- CateringService/web/js/scripts.js
+++ CateringService/web/js/scripts.js
@@ -1,3 +1,10 @@
+//const slike = document.querySelectorAll('img');  
 
+//slike.forEach(img => {
+  //  img.addEventListener('error', function handleError() {
+    //    const defaultSlika =
+      //          './img/svg/minilogo.svg';
 
-
+        //img.src = defaultSlika;
+   // });
+//});
