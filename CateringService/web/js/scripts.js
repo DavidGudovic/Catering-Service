1bd325fc9a3edf10a713b097bf248e6d4ce43b2f
David Gudovic <dgudovic1@gmail.com>
2022-07-17 01:59:44 +0200

    added script for displaying discounted price in cart

--- CateringService/web/js/scripts.js
+++ CateringService/web/js/scripts.js
@@ -3,3 +3,17 @@ function updateUrl(element){
             var link = document.getElementById("a" + element.id);
             link.setAttribute("href", "Korpa?Zahtev=Izmeni&Proizvod="+ element.id +"&Kolicina=" + element.value);  
 }
+function popust(element){
+     let subtotal = document.getElementById("subtotal").textContent;
+     let total = document.getElementById("total");
+    
+     if(element.value > 2){
+         element.value = 2;
+     }
+     if(element.value < 0){
+         element.value = 0;
+     }
+     let popust = element.value * 10;
+     total.textContent = subtotal - ((subtotal/100) * popust);
+             
+}
