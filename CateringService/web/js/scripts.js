0fbbd5b0302fd3efc158d8d7c4955365c4f8cd85
David Gudovic <dgudovic1@gmail.com>
2022-07-19 18:23:50 +0200

    modified max value in popust script

--- CateringService/web/js/scripts.js
+++ CateringService/web/js/scripts.js
@@ -3,16 +3,17 @@ function updateUrl(element){
             var link = document.getElementById("a" + element.id);
             link.setAttribute("href", "Korpa?Zahtev=Izmeni&Proizvod="+ element.id +"&Kolicina=" + element.value);  
 }
+// Skripta dinamicki racuna Total sa popustom u korpi
 function popust(element){
      let subtotal = document.getElementById("subtotal").textContent;
-     let total = document.getElementById("total");
+     let total = document.getElementById("total");  
     
      if(element.value > 2){
-         element.value = 2;
+         element.value = 0;
      }
      if(element.value < 0){
          element.value = 0;
-     }
+     }s
      let popust = element.value * 10;
      total.textContent = subtotal - ((subtotal/100) * popust);
              
