9fc8c18f6ae6d4e965591b51fb7558b63c59790b
David Gudovic <dgudovic1@gmail.com>
2022-07-22 13:12:26 +0200

    added jquery

--- CateringService/web/js/scripts.js
+++ CateringService/web/js/scripts.js
@@ -1,20 +1,60 @@
 // Skripta menja link buttona za menjanje kolicine u korpi
-function updateUrl(element){
-            var link = document.getElementById("a" + element.id);
-            link.setAttribute("href", "Korpa?Zahtev=Izmeni&Proizvod="+ element.id +"&Kolicina=" + element.value);  
+function updateUrl(element) {
+    var link = document.getElementById("a" + element.id);
+    link.setAttribute("href", "Korpa?Zahtev=Izmeni&Proizvod=" + element.id + "&Kolicina=" + element.value);
 }
 // Skripta dinamicki racuna Total sa popustom u korpi
-function popust(element){
-     let subtotal = document.getElementById("subtotal").textContent;
-     let total = document.getElementById("total");  
-    
-     if(element.value > 2){
-         element.value = 0;
-     }
-     if(element.value < 0){
-         element.value = 0;
-     }s
-     let popust = element.value * 10;
-     total.textContent = subtotal - ((subtotal/100) * popust);
-             
+function popust(element) {
+    let subtotal = document.getElementById("subtotal").textContent;
+    let total = document.getElementById("total");
+
+    if (element.value > 2) {
+        element.value = 0;
+    }
+    if (element.value < 0) {
+        element.value = 0;
+    }
+    let popust = element.value * 10;
+    total.textContent = subtotal - ((subtotal / 100) * popust);
+
 }
+
+// Jquery
+
+$(document).ready(function () {
+
+    //Async poziv Proizvod controllera za izmenu proizvoda.
+    $(".btn-izmeni").on("click", function () {
+        $proizvod = event.target.id;
+        $naziv = $("#naziv" + $proizvod).text();
+        $slika = $("#slika" + $proizvod).text();
+        $opis = $("#opis" + $proizvod).text();
+        $cena = $("#cena" + $proizvod).text();
+        $kategorija = $("#kategorija" + $proizvod + " :selected").text();
+        $kategorijaID = $("#kategorija" + $proizvod + " :selected").attr("id");
+
+        if ($.isNumeric($cena)) {
+            $.ajax({
+                type: "POST",
+                data: {proizvod: $proizvod,
+                    naziv: $naziv,
+                    slika: $slika,
+                    opis: $opis,
+                    cena: $cena,
+                    kategorija: $kategorija,
+                    kategorijaID: $kategorijaID},
+                url: "/CateringService/Proizvodi",
+                success: function () {
+                    alert("Proizvod je izmenjen!");
+                },
+                error: function () {
+                    alert("Doslo je do greške");
+                }
+            });
+        } else {
+            alert("Cena mora biti broj");
+        }
+    });
+    
+    // Async poziv za Kategorije kontroler
+});
