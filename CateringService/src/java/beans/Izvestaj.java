677ce8448ca621b4d9b949caaa6f75be379d96a3
David Gudovic <dgudovic1@gmail.com>
2022-07-25 21:26:59 +0200

    started implementing reports

--- CateringService/src/java/beans/Izvestaj.java
+++ CateringService/src/java/beans/Izvestaj.java
@@ -3,21 +3,10 @@ package beans;
 import java.io.Serializable;
 
 public class Izvestaj implements Serializable{
-    private String tekstIzvestaja;
-
+  
     public Izvestaj() {
     }
 
     public Izvestaj(String tekstIzvestaja) {
-        this.tekstIzvestaja = tekstIzvestaja;
     }
-
-    public String getTekstIzvestaja() {
-        return tekstIzvestaja;
-    }
-
-    public void setTekstIzvestaja(String tekstIzvestaja) {
-        this.tekstIzvestaja = tekstIzvestaja;
-    }
-    
 }
--- CateringService/src/java/repository/IzvestajRepository.java
+++ CateringService/src/java/repository/IzvestajRepository.java
@@ -1,35 +1,7 @@
 package repository;
-
-import beans.Izvestaj;
-import interfaces.IRepository;
-import java.sql.SQLException;
-import java.util.List;
-
-public class IzvestajRepository implements IRepository<Izvestaj> {
-
-    @Override
-    public void dodaj(Izvestaj zaDodavanje) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public List<Izvestaj> getSve() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void izmeni(Izvestaj stariT, Izvestaj noviT) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void izbrisi(Izvestaj zaBrisanje) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Izvestaj getJedan(Izvestaj trazeni) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
+/*
+Ne implementira IRepository
+Dohvaca podatke potrebne za izradu izvestaja iz baze
+*/
+public class IzvestajRepository {
 }
